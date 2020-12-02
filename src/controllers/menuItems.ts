6aeba3304805a7423e460b609aaf1c893785897b
Evans <evansm54@GBSFYVL001M7922.local>
2020-12-02 18:15:02 +0000

    update read me

--- src/controllers/menuItems.ts
+++ src/controllers/menuItems.ts
@@ -1,7 +1,10 @@
 import { query } from "../database/createConnection";
 import { Request, Response } from "express";
 
-export const getAllProducts = async (req: Request, res: Response) => {
+export const getAllProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const result = await query("SELECT * FROM menu_items");
     res.send(result.rows);
@@ -11,10 +14,30 @@ export const getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-export const getProductById = async (req: Request, res: Response) => {
+export const getProductById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { id } = req.params;
     const result = await query("SELECT * FROM menu_items WHERE id = $1", [id]);
+    res.send(result.rows[0]);
+  } catch (error) {
+    res.status(500).send({ error: "there has been an error" });
+  }
+};
+
+export const updateProductStock = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { stock } = req.body;
+    const result = await query(
+      "UPDATE menu_items SET stock = $1 WHERE id = $2 RETURNING *",
+      [stock, id]
+    );
     res.send(result.rows[0]);
   } catch (error) {
     res.status(500).send({ error: "there has been an error" });
--- src/routes/menuItems.ts
+++ src/routes/menuItems.ts
@@ -1,7 +1,12 @@
-import { getAllProducts, getProductById } from "../controllers/menuItems";
+import {
+  getAllProducts,
+  getProductById,
+  updateProductStock,
+} from "../controllers/menuItems";
 import { Application } from "express";
 
 export default (app: Application) => {
   app.get("/menu_items", getAllProducts);
   app.get("/menu_items/:id", getProductById);
+  app.get("/menu_items_update/:id", updateProductStock);
 };
